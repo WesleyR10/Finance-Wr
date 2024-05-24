b26218fe13599aa2234e0db03412fff040a106c4
WesleyR10 <wesleyribas2015@gmail.com>
2024-05-24 02:53:03 -0300

    Project setup and authentication with Clerk

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="">hello world</h1>
+      <Button variant="destructive">Testando</Button>
+    </>
+  )
+}
