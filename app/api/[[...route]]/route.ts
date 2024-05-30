72760b155469b231f46eda779ed6fd5455011346
WesleyR10 <wesleyribas2015@gmail.com>
2024-05-30 09:56:15 -0300

    Transaction API + UI - CSV import

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,7 +3,7 @@ import { handle } from 'hono/vercel';
 
 import accounts from './accounts';
 import categories from './categories';
-// import transactions from './transactions';
+import transactions from './transactions';
 // import summary from './summary';
 
 export const runtime = 'edge';
@@ -18,7 +18,7 @@ const routes = app
 //   .route('/summary', summary)
     .route('/accounts', accounts)
     .route('/categories', categories)
-//   .route('/transactions', transactions);
+    .route('/transactions', transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -6,8 +6,8 @@ import { NewAccountSheet } from '@/features/accounts/components/new-account-shee
 import { EditAccountSheet } from '@/features/accounts/components/edit-account-sheet';
 import { NewCategorySheet } from '@/features/categories/components/new-category-sheet';
 import { EditCategorySheet } from '@/features/categories/components/edit-category-sheet';
-// import { NewTransactionSheet } from '@/features/transactions/components/new-transaction-sheet';
-// import { EditTransactionSheet } from '@/features/transactions/components/edit-transaction-sheet';
+import { NewTransactionSheet } from '@/features/transactions/components/new-transaction-sheet';
+import { EditTransactionSheet } from '@/features/transactions/components/edit-transaction-sheet';
 
 export const SheetProvider = () => {
   const isMounted = useMountedState();
@@ -22,10 +22,9 @@ export const SheetProvider = () => {
 
       <NewCategorySheet />
       <EditCategorySheet />
-      {/*
+
       <NewTransactionSheet />
       <EditTransactionSheet />
-      */}
     </>
   );
 };
